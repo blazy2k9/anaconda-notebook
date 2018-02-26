55e64b7302e4aa4775ea0e9288c98f46d9ee97a9
Victor Chima <vchima@spotify.com>
2018-02-27 00:30:16 +0100

    Adds scripts for building and running the docker image

---
+++ docker_run.sh
@@ -0,0 +1,3 @@
+docker run -i -t -p 8888:8888 -v \
+/Users/vchima/personal/workspace/anaconda-notebook/src/notebooks:/home/condauser/notebooks anaconda-notebook \
+/home/condauser/anaconda3/bin/jupyter notebook
